25d83c042fb71ab1807adb657683ffd5f320288b
yf <2652020872@qq.com>
2022-11-08 23:43:57 +0800

    "fib"

--- algorithm/lc/dp/1-215-findKthLargest.go
+++ algorithm/lc/dp/1-215-findKthLargest.go
@@ -8,10 +8,6 @@ import (
 // 注: 你需要找的是数组排序后的第 k 个最大的元素，而不是第 k 个不同的元素。
 // 本题希望我们返回数组排序之后的倒数第 k 个位置。
 
-func main() {
-
-}
-
 // 朴素解法，直接维护一个 K 大的数组，替换掉最小的数
 func findKthLargest(nums []int, k int) int {
 	arr := append([]int{}, nums[:k]...)
