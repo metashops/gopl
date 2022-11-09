de5a519d7d1345e6d7a20014fb6e9a6ead8a3c21
yf <2652020872@qq.com>
2022-11-09 12:05:22 +0800

    "dp"

---
+++ algorithm/lc/dp/3-70-climbStairs.go
@@ -0,0 +1,20 @@
+package main
+
+// func main() {
+// 	res := climbStairs(4)
+// 	fmt.Println(res)
+// }
+
+func climbStairs(n int) int {
+	if n < 2 {
+		return n
+	}
+
+	dp := make([]int, n+1)
+	dp[1] = 1
+	dp[2] = 2
+	for i := 3; i <= n; i++ {
+		dp[i] = dp[i-1] + dp[i-2]
+	}
+	return dp[n]
+}
