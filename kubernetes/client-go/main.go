b9381f266c13733f99fd1594a7731591378b5c80
yf <2652020872@qq.com>
2022-12-07 11:54:07 +0800

    "featinformer"

--- kubernetes/client-go/main.go
+++ kubernetes/client-go/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"time"
 
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/labels"
+	"k8s.io/apimachinery/pkg/util/runtime"
+	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/cache"
 	"k8s.io/client-go/tools/clientcmd"
 )
 
@@ -21,14 +24,36 @@ func main() {
 		panic(err)
 	}
 
-	// 3、
-	pods, err := clientSet.CoreV1().
-		Pods("default").
-		List(context.TODO(), metav1.ListOptions{})
-	if err != nil {
-		panic(err.Error())
+	// 3、初始化shared informer factory以及pod informer
+	factory := informers.NewSharedInformerFactory(clientSet, time.Second*30)
+	podInformer := factory.Core().V1().Pods()
+	informer := podInformer.Informer()
+
+	// 注册informer的自定义ResourceEventHandler
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc:    func(obj interface{}) {},
+		UpdateFunc: func(oldObj, newObj interface{}) {},
+		DeleteFunc: func(obj interface{}) {},
+	})
+
+	// 启动shared informer factory，开始informer的list & watch操作
+	stopper := make(chan struct{})
+	go factory.Start(stopper)
+
+	// 等待informer从kube-apiserver同步资源完成，即informer的list操作获取的对象都存入到informer中的indexer本地缓存中
+	// 或者调用factory.WaitForCacheSync(stopper)
+	if !cache.WaitForCacheSync(stopper, informer.HasSynced) {
+		runtime.HandleError(fmt.Errorf("Timed out waiting for caches to sync"))
+		return
 	}
-	for _, item := range pods.Items {
+	// 创建lister
+	podLister := podInformer.Lister()
+	// 从informer中的indexer本地缓存中获取对象
+	podList, err := podLister.List(labels.Everything())
+	if err != nil {
+		fmt.Println(err)
+	}
+	for _, item := range podList {
 		fmt.Printf("namespace:%v name:%v\n", item.Namespace, item.Name)
 	}
 }
