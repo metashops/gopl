66aa91e720d31bf3ab85f032e9ffc9782286a36f
yf <2652020872@qq.com>
2023-03-01 13:45:28 +0800

    "feat:rust"

--- gobase/map/map.go
+++ gobase/map/map.go
@@ -8,6 +8,33 @@ import (
 // 为什么？：for range map 在开始处理循环逻辑的时候，就做了随机播种
 
 func main() {
-	m := make(map[string]int, 10)
-	fmt.Println(m)
+	// m := map[string]float64{"pi": 3.13}
+	// if v, ok := m["pi"]; ok {
+	// 	fmt.Println(v)
+	// }
+	// Get slices of keys and values from a map
+
+	// myMap := map[string]int{"a": 1, "b": 2, "c": 3}
+	//
+	// keys := make([]string, 0, len(myMap))
+	// vls := make([]int, 0, len(myMap))
+	//
+	// for k, v := range myMap {
+	// 	keys = append(keys, k)
+	// 	vls = append(vls, v)
+	// }
+	//
+	// for _, v := range keys {
+	// 	fmt.Print(v, " ")
+	// }
+
+	var temp []int
+	mp := map[int]int{1: 1, 2: 2, 3: 3, 4: 4, 5: 5}
+	for k := range mp {
+		temp = append(temp, k)
+	}
+
+	for _, v := range temp {
+		fmt.Println(v, mp[v])
+	}
 }
--- kubernetes/client-go/example/clientset/main.go
+++ kubernetes/client-go/example/clientset/main.go
@@ -2,11 +2,15 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
+	virtv1 "kubevirt.io/client-go/api/v1"
+	"kubevirt.io/client-go/kubecli"
 )
 
 func main() {
@@ -32,4 +36,39 @@ func main() {
 	for _, item := range pods.Items {
 		fmt.Printf("namespace:%v name:%v\n", item.Namespace, item.Name)
 	}
+
+	// 获取PVC
+	pvcName := "my-pvc"
+	pvc, err := clientSet.CoreV1().PersistentVolumeClaims("default").Get(context.TODO(), pvcName, metav1.GetOptions{})
+	// Annotations的作用是提供有关对象的附加信息和元数据
+	pvc.Annotations["mycompany.com/cdi"] = "none"
+	pvc.Annotations["mycompany.com/cdi"] = "kubevirt"
+	anntationMap := map[string]interface{}{
+		"metadata": map[string]map[string]string{
+			"annotations": pvc.Annotations,
+		},
+	}
+
+	annotationsBytes, err := json.Marshal(anntationMap)
+
+	ctx := context.Background()
+
+	// 更新PVC对象Annotations
+	_, err := clientSet.CoreV1().PersistentVolumeClaims("default").Patch(ctx, pvcName, types.MergePatchType, annotationsBytes)
+
+	// AddVolume
+	virtClient := kubecli.KubevirtClient
+	err = virtClient.VirtualMachine(namespace).AddVolume(vmId, &virtv1.AddVolumeOptions{
+		Name: volume,
+		Disk: &virtv1.Disk{
+			Name: volume,
+			DiskDevice: virtv1.DiskDevice{
+				Disk: &virtv1.DiskTarget{
+					Bus: diskBus,
+				},
+			},
+		},
+		VolumeSource: vsPersist,
+	})
+
 }
