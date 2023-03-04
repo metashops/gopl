f1c1422afbff3c2dab3b26a1e469e589cdc21c70
yftry <apple@apples-MacBook-Pro.local>
2023-03-04 23:05:25 +0800

    lc

--- algorithm/codeing/array/array_test.go
+++ algorithm/codeing/array/array_test.go
@@ -1,8 +1,15 @@
 package main
 
 import (
-	"testing"
+	`testing`
 )
+
+func TestRemoveElement(t *testing.T) {
+	if ans := RemoveElement([]int{1, 3, 4, 5, 6, 7, 8}, 3); ans != 6 {
+		t.Errorf("expected 1, got %d", ans)
+	}
+
+}
 
 func TestSearch(t *testing.T) {
 	if ans := Search([]int{1, 3, 4, 5, 6, 7, 8}, 3); ans != 1 {
